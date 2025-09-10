1a5808bcbdd2ac3f876bb0d8a3da7d20c4136cfa
Rapha149 <49787110+Rapha149@users.noreply.github.com>
2025-09-10 17:42:25 +0200

    Add auto and check answer command flags.

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
 import Data.Char
 import Data.List.Extra
 import Text.Printf
+import Text.HTML.TagSoup
+import Text.Regex
+import Text.Regex.TDFA
 import Data.Time.Clock.POSIX
 import Control.Monad
 import System.Clipboard
@@ -14,7 +19,7 @@ import System.Exit (exitFailure)
 import Control.Exception
 import Network.HTTP.Conduit
 import Network.HTTP.Types.Status (statusCode)
-import Network.HTTP.Types.Header (hCookie)
+import Network.HTTP.Types.Header (hCookie, hContentType)
 import qualified Configuration.Dotenv as Dotenv
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy.Char8 as L8
@@ -23,28 +28,36 @@ import Util
 import SelectYear
 
 data Source = File | Stdin | Clipboard
-data Args = ArgError String | Options { time :: Bool, year :: Int, day :: Int, part :: Int, source :: Source, extra :: [String]}
+data Flag = MeasureTime | AutoAnswer | CheckAnswer deriving Eq
+data Args = ArgError String | Options { flags :: [Flag], year :: Int, day :: Int, part :: Int, source :: Source, extra :: [String]}
 
 abort :: String -> IO ()
 abort err = hPutStrLn stderr err >> exitFailure
 
 parseArgs :: [String] -> Args
-parseArgs ("-t":xs) = case parseArgs xs of
-                           opts@(Options {}) -> opts { time = True }
-                           err -> err
+parseArgs (('-':flagOpt):xs) | flagOpt `notElem` ["t", "a", "ca"] = ArgError "Invalid flag argument."
+                             | otherwise = let flag = case flagOpt of
+                                                           "t" -> MeasureTime
+                                                           "a" -> AutoAnswer
+                                                           "ca" -> CheckAnswer
+                                                           _ -> error "Unknown flag argument."
+                                           in case parseArgs xs of
+                                                   Options {..} -> Options { flags = flag : flags, .. }
+                                                   err -> err
 parseArgs [year, day, part] = parseArgs [year, day, part, "file"]
 parseArgs (year:day:part:_) | not $ all (all isDigit) [year, day, part] = ArgError "Specify numbers for year, day and part."
 parseArgs (_:_:_:source:_) | lower source `notElem` ["file", "stdin", "clip"] = ArgError "Specify 'file', 'stdin' or 'clip' for the source."
-parseArgs (year:day:part:source:extra) = Options { time = False,
-                                                   year = read year,
-                                                   day = read day,
-                                                   part = read part,
-                                                   source = case lower source of
-                                                               "file" -> File
-                                                               "stdin" -> Stdin
-                                                               "clip" -> Clipboard
-                                                               _ -> error "Unknown source argument.",
-                                                   extra }
+parseArgs (year:day:part:source:extra) | lower source `notElem` ["file", "stdin", "clip"] = ArgError "Invalid source argument."
+                                       | otherwise = Options { flags = [],
+                                                               year = read year,
+                                                               day = read day,
+                                                               part = read part,
+                                                               source = case lower source of
+                                                                             "file" -> File
+                                                                             "stdin" -> Stdin
+                                                                             "clip" -> Clipboard
+                                                                             _ -> error "Unknown source argument.",
+                                                               extra }
 parseArgs _ = ArgError "Illegal number of arguments."
 
 run :: Args -> IO ()
@@ -53,11 +66,19 @@ run Options {..} = do
     input <- loadInput year day source
     putStrLn $ printf "Calculating result for year %d, day %d, part %d..." year day part
     startTime <- getPOSIXTime
-    case selectYear year day part $ extra ++ lines input of
+    let result = selectYear year day part $ extra ++ lines input
+        answer = case result of
+                      V value -> show value
+                      Msg msg -> msg
+                      Error _ -> error "Result is an error."
+    case result of
          V value -> putStrLn $ printf "Result: %d" value
          Msg msg -> putStrLn msg
          Error err -> abort err
-    when time $ printTimeDiff startTime
+
+    when (MeasureTime `elem` flags) $ printTimeDiff startTime
+    when (AutoAnswer `elem` flags) $ autoAnswer year day part answer
+    when (CheckAnswer `elem` flags && AutoAnswer `notElem` flags) $ checkAnswer year day part answer
 
 printTimeDiff :: POSIXTime -> IO ()
 printTimeDiff start = do
@@ -90,30 +111,79 @@ loadInput year day _ = do
             putStrLn $ printf "Failed to read the file: \"%s\"." path
             exitFailure
 
-fetchInput :: Int -> Int -> IO String
-fetchInput year day = do
-    catchIOError (Dotenv.loadFile Dotenv.defaultConfig) (\_ -> putStrLn "Warning: .env not found, skipping.")
+sessionCookie :: IO String
+sessionCookie = do
+    catchIOError (Dotenv.loadFile Dotenv.defaultConfig) (\_ -> putStrLn "Warning: .env not found.")
     cookie <- lookupEnv "COOKIE"
-    session <- case cookie of
+    case cookie of
         Just session -> return session
         Nothing -> do
-            hPutStrLn stderr "COOKIE environment variable missing"
+            hPutStrLn stderr "COOKIE environment variable missing."
             exitFailure
 
+fetchInput :: Int -> Int -> IO String
+fetchInput year day = do
+    session <- sessionCookie
     manager <- newManager tlsManagerSettings
-    req <- parseRequest $ printf "https://adventofcode.com/%d/day/%d/input" year day
-    let reqWithHeader = req { requestHeaders = [(hCookie, BS.pack $ "session=" <> session)] }
-    response <- httpLbs reqWithHeader manager
+    initReq <- parseRequest $ printf "https://adventofcode.com/%d/day/%d/input" year day
+    let req = initReq { requestHeaders = [(hCookie, BS.pack $ "session=" <> session)] }
+    response <- httpLbs req manager
     let body = L8.unpack $ responseBody response
     case statusCode $ responseStatus response of
         200 -> return body
         400 -> do
-            hPutStrLn stderr $ printf "Server responded with 400 is your session cookie still valid? (%s)" $ trim body
+            hPutStrLn stderr $ printf "[FetchInput] Server responded with 400 is your session cookie still valid? (%s)" $ trim body
             exitFailure
         code -> do
-            hPutStrLn stderr $ printf "Code %d: %s" code body
+            hPutStrLn stderr $ printf "[FetchInput] Code %d: %s" code body
             exitFailure
 
+autoAnswer :: Int -> Int -> Int -> String -> IO ()
+autoAnswer year day part answer = do
+    session <- sessionCookie
+    manager <- newManager tlsManagerSettings
+    initReq <- parseRequest $ printf "https://adventofcode.com/%d/day/%d/answer" year day
+    let req = initReq { method = "POST",
+                        requestHeaders = [(hCookie, BS.pack $ "session=" <> session),
+                                          (hContentType, "application/x-www-form-urlencoded")],
+                        requestBody = RequestBodyBS (BS.pack $ printf "level=%d&answer=%s" part answer) }
+    response <- httpLbs req manager
+    let body = L8.unpack $ responseBody response
+    case statusCode $ responseStatus response of
+        200 -> let tags = parseTags body
+                   text = trim $ innerText $ takeWhile (~/= ("</main>" :: String)) $ drop 1 $ dropWhile (~/= ("<main>" :: String)) tags
+                   prefix = hd $ splitOn "  " text
+               in case prefix of
+                       "That's the right answer!" -> putStrLn $ "\n[AutoAnswer] " <> prefix
+                       "You don't seem to be solving the right level." -> checkAnswer year day part answer
+                       _ -> putStrLn $ "\n[AutoAnswer] " <> unwords (words $ subRegex (mkRegex " *\\[[^]]+\\]") text "")
+        400 -> do
+            hPutStrLn stderr $ printf "[AutoAnswer] Server responded with 400 is your session cookie still valid? (%s)" $ trim body
+            exitFailure
+        code -> do
+            hPutStrLn stderr $ printf "[AutoAnswer] Code %d: %s" code body
+            exitFailure
+
+checkAnswer :: Int -> Int -> Int -> String -> IO ()
+checkAnswer year day part answer = do
+    session <- sessionCookie
+    manager <- newManager tlsManagerSettings
+    initReq <- parseRequest $ printf "https://adventofcode.com/%d/day/%d" year day
+    let req = initReq { requestHeaders = [(hCookie, BS.pack $ "session=" <> session)] }
+    response <- httpLbs req manager
+    let body = L8.unpack $ responseBody response
+    case statusCode $ responseStatus response of
+        200 -> let answers = map lst $ body =~ ("<p>Your puzzle answer was <code>(.+)</code>.</p>" :: String)
+               in putStrLn $ "\n[CheckAnswer] " <> case (== answer) <$> answers !? (part - 1) of
+                                                        Just True -> "That's the right answer!"
+                                                        Just False -> "That's not the right answer."
+                                                        Nothing -> "Puzzle not solved yet."
+        400 -> do
+            hPutStrLn stderr $ printf "[CheckAnswer] Server responded with 400 is your session cookie still valid? (%s)" $ trim body
+            exitFailure
+        code -> do
+            hPutStrLn stderr $ printf "[CheckAnswer] Code %d: %s" code body
+            exitFailure
 
 main :: IO ()
 main = do
