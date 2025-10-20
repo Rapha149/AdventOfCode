f33b039f5127824b2e6d2d9d445c71c49e5d7b25
Rapha149 <49787110+Rapha149@users.noreply.github.com>
2025-10-20 22:07:29 +0200

    Implement 2018/15-16.

--- app/Years/Year2018/Day15.hs
+++ app/Years/Year2018/Day15.hs
@@ -1,9 +1,68 @@
 module Years.Year2018.Day15 (part1, part2) where
 
 import Util.Util
+import Data.Maybe
+import Data.List.Extra
+import Data.Tuple.Extra
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+
+type State = (Set Vec, Map Vec (Bool, Int))
+data Status = Ongoing | Finished | ElfDied
+
+parseInput :: [String] -> State
+parseInput input = (Map.keysSet $ Map.filter (== '.') grid, Map.mapMaybe (\case 'E' -> Just (True, 200)
+                                                                                'G' -> Just (False, 200)
+                                                                                _ -> Nothing) grid)
+    where grid = Map.fromList [((r, c), x) | (r, row) <- zip [0..] input, (c, x) <- zip [0..] row]
+
+bfs :: Set Vec -> Set Vec -> [[Vec]] -> Set Vec -> Maybe Vec
+bfs _ _ [] _ = Nothing
+bfs open ends (path:xs) seen | Set.member pos ends = Just $ lst $ ini path
+                             | otherwise = bfs open ends (xs ++ map (: path) next) (foldr Set.insert seen next)
+    where pos = hd path
+          next = [pos' | delta <- [{- reading order! -} (-1, 0), (0, -1), (0, 1), (1, 0)], let pos' = onBoth (+) pos delta,
+                         Set.member pos' open, Set.notMember pos' seen]
+
+attack :: Int -> State -> Vec -> (Maybe Vec, State)
+attack elfPower (open, units) pos | null targets = (Nothing, (open, units))
+                                  | health > power = (Nothing, (open, Map.insert target (not elf, health - power) units))
+                                  | otherwise = (Just target, (Set.insert target open, Map.delete target units))
+    where (elf, _) = units Map.! pos
+          power = if elf then elfPower else 3
+          targets = Map.filter ((/= elf) . fst) $ Map.restrictKeys units $ Set.fromList [onBoth (+) pos delta | delta <- [(-1, 0), (0, -1), (0, 1), (1, 0)]]
+          (target, health) = minimumOn swap $ Map.toList $ Map.map snd targets
+
+combatRound :: Int -> State -> [Vec] -> (State, Status)
+combatRound _ state [] = (state, Ongoing)
+combatRound elfPower (open, units) (pos:xs) | null targets = ((open, units), Finished)
+                                            | Set.member pos adjacent = handleAttackResult $ attack elfPower (open, units) pos
+                                            | null reachable = combatRound elfPower (open, units) xs
+                                            | otherwise = case bfsResult of
+                                                               Nothing -> combatRound elfPower (open, units) xs
+                                                               Just move -> handleAttackResult $ attack elfPower (Set.insert pos $ Set.delete move open, Map.insert move unit $ Map.delete pos units) move
+    where unit@(elf, _) = units Map.! pos
+          targets = Map.keys $ Map.filter ((/= elf) . fst) units
+          adjacent = Set.fromList $ concatMap (\p -> [onBoth (+) p d | d <- [(-1, 0), (0, -1), (0, 1), (1, 0)]]) targets
+          reachable = Set.intersection adjacent open
+          bfsResult = bfs open reachable [[pos]] (Set.singleton pos)
+          handleAttackResult :: (Maybe Vec, State) -> (State, Status)
+          handleAttackResult (Nothing, state) = combatRound elfPower state xs
+          handleAttackResult (Just dead, state) | not elf && elfPower > 3 = (state, ElfDied)
+                                                | otherwise = combatRound elfPower state $ delete dead xs
+
+getBattleOutcome :: Int -> Int -> State -> Maybe Int
+getBattleOutcome elfPower i state = case status of
+                                         Ongoing -> getBattleOutcome elfPower (i + 1) state'
+                                         Finished -> Just $ i * sum (Map.map snd $ snd state')
+                                         ElfDied -> Nothing
+      where (state', status) = combatRound elfPower state $ Map.keys $ snd state
 
 part1 :: Solution
-part1 = undefined
+part1 = V . fromJust . getBattleOutcome 3 0 . parseInput
 
 part2 :: Solution
-part2 = undefined
+part2 input = V $ hd $ mapMaybe (\elfPower -> getBattleOutcome elfPower 0 state) [4..]
+    where state = parseInput input
--- app/Years/Year2018/Day16.hs
+++ app/Years/Year2018/Day16.hs
@@ -1,9 +1,83 @@
 module Years.Year2018.Day16 (part1, part2) where
 
 import Util.Util
+import Data.Bits
+import Data.Word
+import Data.List.Split
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+
+data Registers = Registers { r0 :: Int, r1 :: Int, r2 :: Int, r3 :: Int } deriving (Show, Eq)
+data Args = Args { a :: Int, b :: Int, c :: Int } deriving Show
+data Sample = Sample { op :: Int, args :: Args, before :: Registers, after :: Registers } deriving Show
+
+parseInput :: [String] -> ([Sample], [(Int, Args)])
+parseInput input = (samples, map parseInstruction programInput)
+    where (sampleInput, programInput) = pair $ splitOn ["", "", ""] input
+          samples = [Sample {..} | [beforeStr, instr, afterStr] <- splitOn [""] sampleInput, let (op, args) = parseInstruction instr,
+                                   let before = parseRegisters beforeStr, let after = parseRegisters afterStr]
+          parseRegisters :: String -> Registers
+          parseRegisters line = case read $ concat $ tl $ words line of
+                                     [a, b, c, d] -> Registers a b c d
+                                     _ -> error "Invalid registers input."
+          parseInstruction :: String -> (Int, Args)
+          parseInstruction line = case map read $ words line of
+                                       [op, a, b, c] -> (op, Args a b c)
+                                       _ -> error "Invalid instruction input."
+
+get :: Int -> Registers -> Int
+get 0 = r0
+get 1 = r1
+get 2 = r2
+get 3 = r3
+get _ = error "Invalid register."
+
+set :: Int -> Int -> Registers -> Registers
+set 0 val regs = regs { r0 = val }
+set 1 val regs = regs { r1 = val }
+set 2 val regs = regs { r2 = val }
+set 3 val regs = regs { r3 = val }
+set _ _ _ = error "Invalid register."
+
+instruction :: Word16 -> Args -> Registers -> Registers
+instruction bits Args{..} regs = set c (case bits of
+                                  {- addr -} 0x0001 -> aVal + bVal
+                                  {- addi -} 0x0002 -> aVal + b
+                                  {- mulr -} 0x0004 -> aVal * bVal
+                                  {- muli -} 0x0008 -> aVal * b
+                                  {- banr -} 0x0010 -> aVal .&. bVal
+                                  {- bani -} 0x0020 -> aVal .&. b
+                                  {- borr -} 0x0040 -> aVal .|. bVal
+                                  {- bori -} 0x0080 -> aVal .|. b
+                                  {- setr -} 0x0100 -> aVal
+                                  {- seti -} 0x0200 -> a
+                                  {- gtir -} 0x0400 -> fromEnum $ a > bVal
+                                  {- gtri -} 0x0800 -> fromEnum $ aVal > b
+                                  {- gtrr -} 0x1000 -> fromEnum $ aVal > bVal
+                                  {- eqir -} 0x2000 -> fromEnum $ a == bVal
+                                  {- eqri -} 0x4000 -> fromEnum $ aVal == b
+                                  {- eqrr -} 0x8000 -> fromEnum $ aVal == bVal
+                                             _ -> error "Invalid bitmask") regs
+    where aVal = get a regs
+          bVal = get b regs
+
+getMatchingBits :: Sample -> [Word16]
+getMatchingBits Sample {..} = [bits | i <- [0..15], let bits = setBit 0 i, instruction bits args before == after]
 
 part1 :: Solution
-part1 = undefined
+part1 = V . length . filter ((>= 3) . length . getMatchingBits) . fst . parseInput
+
+
+getOpcodeMap :: [Sample] -> Map Int Word16 -> Map Int Word16
+getOpcodeMap [] masks | 0 `elem` masks = error "At least one opcode has no possibilities."
+                      | null masks = Map.empty
+                      | null determined = error "Some opcodes have multiple possibilities."
+                      | otherwise = Map.union determined $ getOpcodeMap [] $ Map.map (.&. mask) rest
+    where (determined, rest) = Map.partition ((== 1) . popCount) masks
+          mask = complement $ foldr1 (.|.) determined
+getOpcodeMap (sample:xs) masks = getOpcodeMap xs $ Map.adjust (.&. foldr1 (.|.) (getMatchingBits sample)) sample.op masks
 
 part2 :: Solution
-part2 = undefined
+part2 input = V $ r0 $ foldl (\regs (op, args) -> instruction (opcodes Map.! op) args regs) (Registers 0 0 0 0) program
+    where (samples, program) = parseInput input
+          opcodes = getOpcodeMap samples (Map.fromList $ map (, 0xFFFF) [0..15])
