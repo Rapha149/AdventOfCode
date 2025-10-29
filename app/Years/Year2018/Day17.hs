aa10aad69cd8a55bb38b4ab3e609db3538e726c1
Rapha149 <49787110+Rapha149@users.noreply.github.com>
2025-10-29 23:21:23 +0100

    Implement 2018/17-18.

--- app/Years/Year2018/Day17.hs
+++ app/Years/Year2018/Day17.hs
@@ -1,9 +1,49 @@
 module Years.Year2018.Day17 (part1, part2) where
 
 import Util.Util
+import Data.List.Split
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+
+data Tile = Wall | Flowing | Settled deriving (Show, Eq)
+
+parseInput :: [String] -> Map Vec Tile
+parseInput [] = Map.empty
+parseInput (('x':'=':l):ls) = Map.fromList [((x, y), Wall) | y <- [y1..y2]] `Map.union` parseInput ls
+    where (xInput, yInput) = pair $ splitOn ", "l
+          x = read xInput
+          (y1, y2) = pair $ map read $ splitOn ".." $ drop 2 yInput
+parseInput (('y':'=':l):ls) = Map.fromList [((x, y), Wall) | x <- [x1..x2]] `Map.union` parseInput ls
+    where (yInput, xInput) = pair $ splitOn ", " l
+          y = read yInput
+          (x1, x2) = pair $ map read $ splitOn ".." $ drop 2 xInput
+parseInput _ = error "Invalid input"
+
+flow :: Int -> Map Vec Tile -> Vec -> Map Vec Tile
+flow maxY grid (x, y) | y >= maxY = grid
+                      | gridLR Map.! (x, y + 1) == Flowing = gridLR
+                      | otherwise = case liftA2 Map.union (getSettledWater x 1) (getSettledWater x (-1)) of
+                                         Nothing -> gridLR
+                                         Just m -> Map.union m gridLR
+    where gridD = flowIfEmpty grid (x, y + 1)
+          gridLR | maybe True (== Flowing) $ gridD Map.!? (x, y + 1) = gridD
+                 | otherwise = flowIfEmpty (flowIfEmpty gridD (x - 1, y)) (x + 1, y)
+          flowIfEmpty :: Map Vec Tile -> Vec -> Map Vec Tile
+          flowIfEmpty grid' pos | pos `Map.notMember` grid' = flow maxY (Map.insert pos Flowing grid') pos
+                                | otherwise = grid'
+          getSettledWater :: Int -> Int -> Maybe (Map Vec Tile)
+          getSettledWater x' dx = case gridLR Map.!? (x', y) of
+                                       Nothing -> Nothing
+                                       Just Wall -> Just Map.empty
+                                       _ -> Map.insert (x', y) Settled <$> getSettledWater (x' + dx) dx
+
+getWaterLocations :: [String] -> Map Vec Tile
+getWaterLocations input = Map.filterWithKey (\(_, y) t -> y >= minY && t /= Wall) $ flow maxY grid (500, 0)
+    where grid = parseInput input
+          (_, (minY, maxY)) = getBounds $ Map.keys grid
 
 part1 :: Solution
-part1 = undefined
+part1 = V . length . getWaterLocations
 
 part2 :: Solution
-part2 = undefined
+part2 = V . length . Map.filter (== Settled) . getWaterLocations
--- app/Years/Year2018/Day18.hs
+++ app/Years/Year2018/Day18.hs
@@ -1,9 +1,45 @@
 module Years.Year2018.Day18 (part1, part2) where
 
 import Util.Util
+import Data.Maybe
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+
+data Acre = OpenGround | Trees | Lumberyard deriving (Show, Eq, Ord)
+type Area = Map Vec Acre
+
+parseInput :: [String] -> Area
+parseInput input = Map.fromList [((x, y), case c of
+                                               '.' -> OpenGround
+                                               '|' -> Trees
+                                               '#' -> Lumberyard
+                                               _ -> error "Invalid input.") | (y, row) <- zip [0..] input, (x, c) <- zip [0..] row]
+
+oneMinute :: Area -> Area
+oneMinute area = Map.mapWithKey change area
+    where change :: Vec -> Acre -> Acre
+          change (x, y) acre = let adjacent = mapMaybe (area Map.!?) [(x + dx, y + dy) | dx <- [-1..1], dy <- [-1..1], (dx, dy) /= (0, 0)]
+                               in case acre of
+                                       OpenGround -> if length (filter (== Trees) adjacent) >= 3 then Trees else OpenGround
+                                       Trees -> if length (filter (== Lumberyard) adjacent) >= 3 then Lumberyard else Trees
+                                       Lumberyard -> if Trees `elem` adjacent && Lumberyard `elem` adjacent then Lumberyard else OpenGround
+
+afterMinutes :: Int -> Area -> Area
+afterMinutes n = foldr (.) id $ replicate n oneMinute
+
+getResourceValue :: Area -> Int
+getResourceValue area = length (Map.filter (== Trees) area) * length (Map.filter (== Lumberyard) area)
 
 part1 :: Solution
-part1 = undefined
+part1 = V . getResourceValue . afterMinutes 10 . parseInput
+
+
+getLoop :: Area -> Map Area Int -> Int -> (Int, Int)
+getLoop area history i = case history Map.!? area of
+                           Just j -> (j, i - j)
+                           Nothing -> getLoop (oneMinute area) (Map.insert area i history) (i + 1)
 
 part2 :: Solution
-part2 = undefined
+part2 input = V $ getResourceValue $ afterMinutes ((1000000000 - start) `mod` size) $ afterMinutes start area
+    where area = parseInput input
+          (start, size) = getLoop area Map.empty 0
