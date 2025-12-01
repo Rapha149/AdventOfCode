43e246952327dfc91bfa8a772e92198825db990b
Rapha149 <49787110+Rapha149@users.noreply.github.com>
2025-12-01 16:48:15 +0100

    Implement 2025/1.

--- app/Years/Year2025/Day01.hs
+++ app/Years/Year2025/Day01.hs
@@ -2,8 +2,21 @@ module Years.Year2025.Day01 (part1, part2) where
 
 import Util.Util
 
+parseInput :: [String] -> [Int]
+parseInput [] = []
+parseInput (('L':n):xs) = (-read n) : parseInput xs
+parseInput (('R':n):xs) = read n : parseInput xs
+parseInput _ = error "Invalid input."
+
 part1 :: Solution
-part1 = undefined
+part1 = V . length . filter (== 0) . scanl ((`mod` 100) .: (+)) 50 . parseInput
+
+
+getZeroCounts :: Int -> [Int] -> Int
+getZeroCounts _ [] = 0
+getZeroCounts dial (0:xs) = getZeroCounts dial xs
+getZeroCounts dial (x:xs) = getZeroCounts dial' (x - signum x:xs) + if dial' == 0 then 1 else 0
+    where dial' = (dial + signum x) `mod` 100
 
 part2 :: Solution
-part2 = undefined
+part2 = V . getZeroCounts 50 . parseInput
