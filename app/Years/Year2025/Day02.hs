e16b5723ef788f959ba102d3ca5053c318e32115
Rapha149 <49787110+Rapha149@users.noreply.github.com>
2025-12-02 09:47:25 +0100

    Implement 2025/2.

--- app/Years/Year2025/Day02.hs
+++ app/Years/Year2025/Day02.hs
@@ -1,9 +1,24 @@
 module Years.Year2025.Day02 (part1, part2) where
 
 import Util.Util
+import Data.List.Split
+import Text.Regex.PCRE
+
+parseInput :: [String] -> [Int]
+parseInput = concatMap parseRange . splitOn "," . concat
+    where parseRange :: String -> [Int]
+          parseRange s = case splitOn "-" s of
+                              [a, b] -> [read a..read b]
+                              _ -> error "Invalid input."
+
+-- much faster than a regex for part 1
+isInvalid :: Int -> Bool
+isInvalid n = take mid s == drop mid s
+    where s = show n
+          mid = length s `div` 2
 
 part1 :: Solution
-part1 = undefined
+part1 = V . sum . filter isInvalid . parseInput
 
 part2 :: Solution
-part2 = undefined
+part2 = V . sum . filter ((=~ "^(.+)\\1+$") . show) . parseInput
