a64e783c7739ecb109c8445301e3bf77648ed486
Rapha149 <49787110+Rapha149@users.noreply.github.com>
2025-12-03 07:58:19 +0100

    Implement 2025/3.

--- app/Years/Year2025/Day03.hs
+++ app/Years/Year2025/Day03.hs
@@ -1,9 +1,16 @@
 module Years.Year2025.Day03 (part1, part2) where
 
 import Util.Util
+import Data.Char
+import Data.List.Extra
+
+getMaxJoltage :: Int -> [Int] -> Int
+getMaxJoltage 0 _ = 0
+getMaxJoltage n xs = x * 10 ^ (n - 1) + getMaxJoltage (n - 1) (tl $ dropWhile (/= x) xs)
+    where x = maximum $ dropEnd (n - 1) xs
 
 part1 :: Solution
-part1 = undefined
+part1 = V . sumOn' (getMaxJoltage 2 . map digitToInt)
 
 part2 :: Solution
-part2 = undefined
+part2 = V . sumOn' (getMaxJoltage 12 . map digitToInt)
