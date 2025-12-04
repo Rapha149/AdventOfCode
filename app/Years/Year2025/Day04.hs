6bb92385fde703a70dd3242a8a7d0609a8bada02
Rapha149 <49787110+Rapha149@users.noreply.github.com>
2025-12-04 08:29:54 +0100

    Implement 2025/4.

--- app/Years/Year2025/Day04.hs
+++ app/Years/Year2025/Day04.hs
@@ -1,9 +1,23 @@
 module Years.Year2025.Day04 (part1, part2) where
 
 import Util.Util
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+parseInput :: [String] -> Set Vec
+parseInput input = Set.fromList [(r, c) | (r, row) <- zip [0..] input, (c, '@') <- zip [0..] row]
+
+getAccessible :: Set Vec -> Set Vec
+getAccessible rolls = Set.filter (\(r, c) -> length [True | dr <- [-1..1], dc <- [-1..1], (dr, dc) /= (0, 0), (r + dr, c + dc) `Set.member` rolls] < 4) rolls
 
 part1 :: Solution
-part1 = undefined
+part1 = V . length . getAccessible . parseInput
+
+
+countAccessible :: Set Vec -> Int
+countAccessible rolls | null accessible = 0
+                      | otherwise = length accessible + countAccessible (rolls `Set.difference` accessible)
+    where accessible = getAccessible rolls
 
 part2 :: Solution
-part2 = undefined
+part2 = V . countAccessible . parseInput
