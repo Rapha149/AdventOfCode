cf57ae38a499d327b81aaa9c3b425b3a3355c7da
Rapha149 <49787110+Rapha149@users.noreply.github.com>
2025-12-06 10:21:44 +0100

    Implement 2025/6.

--- app/Years/Year2025/Day06.hs
+++ app/Years/Year2025/Day06.hs
@@ -1,9 +1,19 @@
 module Years.Year2025.Day06 (part1, part2) where
 
 import Util.Util
+import Data.List.Extra
+import Data.Tuple.Extra
+
+getGrandTotal :: ([String] -> (Char, [String])) -> [String] -> Int
+getGrandTotal f = sumOn' (solve . f) . split (all (== ' ')) . transpose
+    where solve :: (Char, [String]) -> Int
+          solve (op, xs) = (case op of
+                                 '+' -> sum
+                                 '*' -> product
+                                 _ -> error "Unknown operation") $ map read xs
 
 part1 :: Solution
-part1 = undefined
+part1 = V . getGrandTotal ((hd . lst &&& ini) . transpose)
 
 part2 :: Solution
-part2 = undefined
+part2 = V . getGrandTotal (lst . hd &&& map ini)
